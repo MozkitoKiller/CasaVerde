7bdbffdfa044281a7bbbd5d72e37267dd5496bd9
João Bertuga <95227813+MozkitoKiller@users.noreply.github.com>
2022-01-11 10:18:44 -0300

    Add files via upload

---
+++ casa verde/js/email.js
@@ -0,0 +1,8 @@
+var assinar = document.querySelector(".botao")
+var email = document.querySelector(".input--email")
+
+assinar.addEventListener("click", (event) => {
+         event.preventDefault();
+        alert("um E-mail foi enviado para o endereço informado"  )
+})
+
